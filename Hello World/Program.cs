bb8b82541b2e0a146639987637d7d4a6929a92c2
Diego Arriaga <dielarri@ut.utm.edu>
2021-09-02 16:47:56 -0500

    Guessing game

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -10,7 +10,38 @@ namespace Hello_World
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var random = new Random();
+
+			int answer = random.Next(101);
+
+			Console.WriteLine("Im thinking about a number between 0 and 100");
+			int guess;
+			guess = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine("Your guess is " + guess);
+
+			bool win = false;
+			while (!win) {
+				if (guess == answer)
+				{
+					win = true;
+				}
+				if (guess > answer)
+				{
+					Console.WriteLine("Too High");
+					Console.WriteLine("Try Again");
+					guess = int.Parse(Console.ReadLine());
+				}
+				if (guess < answer)
+				{
+					Console.WriteLine("Too Low");
+					Console.WriteLine("Try Again");
+					guess = int.Parse(Console.ReadLine());
+				}
+			}
+
+			Console.WriteLine("The correct answer is " + answer);
+
 			Console.ReadLine();
 
 		}
